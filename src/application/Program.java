b0986d83597cb7147820a4e37021d4d2afdf9c92
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2021-07-25 17:47:30 -0300

    Solução proposta pelo professor, utilizando 'addAll'

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Locale;
 import java.util.Scanner;
 import java.util.Set;
@@ -13,34 +14,41 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Set<Studants> set = new HashSet<Studants>();
+		Set<Integer> a = new HashSet<Integer>();
+		Set<Integer> b = new HashSet<Integer>();
+		Set<Integer> c = new HashSet<Integer>();
 		
 		System.out.print("Teache name: ");
-		String teacheName = sc.nextLine();		
+		String teacheName = sc.nextLine();	
 		
-		System.out.print("How many students for course A? " );
-		Integer quantityStudants = sc.nextInt();		
-		for(int i = 1; i <= quantityStudants; i++) {	
-			int studantCode = sc.nextInt();
-			set.add(new Studants(studantCode));
+		System.out.print("How many students for course A? ");
+		int n = sc.nextInt();
+		for(int i = 1; i <= n; i++) {
+			int number = sc.nextInt();
+			a.add(number);
 		}
 		
-		System.out.print("How many students for course B? " );
-		Integer quantityStudantsB = sc.nextInt();		
-		for(int i = 1; i <= quantityStudantsB; i++) {	
-			int studantCode = sc.nextInt();
-			set.add(new Studants(studantCode));
+		System.out.print("How many students for course B? ");
+		n = sc.nextInt();
+		for(int i = 1; i <= n; i++) {
+			int number = sc.nextInt();
+			a.add(number);
 		}
 		
-		System.out.print("How many students for course C? " );
-		Integer quantityStudantsC = sc.nextInt();		
-		for(int i = 1; i <= quantityStudantsC; i++) {	
-			int studantCode = sc.nextInt();
-			set.add(new Studants(studantCode));
+		System.out.print("How many students for course C? ");
+		n = sc.nextInt();
+		for(int i = 1; i <= n; i++) {
+			int number = sc.nextInt();
+			a.add(number);
 		}
 		
-		System.out.println("Teacher: " + teacheName);
-		System.out.println("Total students: " + set.size());
+		Set<Integer> total = new HashSet<Integer>(a);
+		total.addAll(b);
+		total.addAll(c);
+		
+		System.out.println("Total studants: " + total.size());
+		
+
 		
 		
 		sc.close();
